f28e3efa58fb8cde53aeed2bdf2ea50544ae70a0
tokartheekdevops <tokartheekdevops@gmail.com>
2026-08-26 03:04:39 +0530

    Add public subnets for VPC configuration

---
+++ subnet.tf
@@ -0,0 +1,23 @@
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_1_cidr
+  availability_zone       = var.availability_zone_1
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name      = "${var.project_name}-public-subnet-1"
+    ManagedBy = "Terraform"
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_2_cidr
+  availability_zone       = var.availability_zone_2
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name      = "${var.project_name}-public-subnet-2"
+    ManagedBy = "Terraform"
+  }
+}
