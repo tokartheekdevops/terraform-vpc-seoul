77d4d126de4163258d4dca4f2d3ba57df8ec22b6
tokartheekdevops <tokartheekdevops@gmail.com>
2026-08-26 03:05:35 +0530

    Add public route table and associations for subnets

---
+++ route_table.tf
@@ -0,0 +1,23 @@
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name      = "${var.project_name}-public-route-table"
+    ManagedBy = "Terraform"
+  }
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
+}
