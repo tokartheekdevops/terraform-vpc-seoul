68437de4dde6a06e6e10e3c349762499da92c344
tokartheekdevops <tokartheekdevops@gmail.com>
2026-08-26 03:40:49 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -18,6 +18,11 @@ output "internet_gateway_id" {
   value       = aws_internet_gateway.main.id
 }
 
+output "route_table_id" {
+  description = "Public route table ID"
+  value       = aws_route_table.public.id
+}
+
 output "security_group_id" {
   description = "Web security group ID"
   value       = aws_security_group.web.id
@@ -29,11 +34,11 @@ output "ec2_instance_id" {
 }
 
 output "ec2_public_ip" {
-  description = "Public IP of the web server"
+  description = "Public IP of the EC2 instance"
   value       = aws_instance.web.public_ip
 }
 
 output "ec2_public_dns" {
-  description = "Public DNS of the web server"
+  description = "Public DNS of the EC2 instance"
   value       = aws_instance.web.public_dns
 }
