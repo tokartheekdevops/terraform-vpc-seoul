f1f6dba9d1b898ec2de9fa891aff2fd160bdc9e7
tokartheekdevops <tokartheekdevops@gmail.com>
2026-08-26 03:36:05 +0530

    Refine variable descriptions in variables.tf
    Updated descriptions for VPC and subnet variables to improve clarity.

--- variables.tf
+++ variables.tf
@@ -11,31 +11,31 @@ variable "project_name" {
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for VPC"
+  description = "CIDR block for the VPC"
   type        = string
   default     = "10.20.0.0/16"
 }
 
 variable "public_subnet_1_cidr" {
-  description = "CIDR for first public subnet"
+  description = "CIDR block for public subnet 1"
   type        = string
   default     = "10.20.1.0/24"
 }
 
 variable "public_subnet_2_cidr" {
-  description = "CIDR for second public subnet"
+  description = "CIDR block for public subnet 2"
   type        = string
   default     = "10.20.2.0/24"
 }
 
 variable "availability_zone_1" {
-  description = "First Seoul availability zone"
+  description = "First availability zone"
   type        = string
   default     = "ap-northeast-2a"
 }
 
 variable "availability_zone_2" {
-  description = "Second Seoul availability zone"
+  description = "Second availability zone"
   type        = string
   default     = "ap-northeast-2b"
 }
@@ -44,16 +44,4 @@ variable "instance_type" {
   description = "EC2 instance type"
   type        = string
   default     = "t3.micro"
-}
-
-variable "key_name" {
-  description = "EC2 key pair name"
-  type        = string
-  default     = "terraform-seoul-key"
-}
-
-variable "public_key" {
-  description = "SSH public key used to create the EC2 key pair"
-  type        = string
-  sensitive   = false
 }
